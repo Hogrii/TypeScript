1b1d491a5f228a9c06fa5cc54312b1986d6d5c06
jinho <hosboy93@naver.com>
2022-03-30 16:14:51 +0900

    typescript variables

--- src/variables.ts
+++ src/variables.ts
@@ -16,8 +16,15 @@ function outer(){
         // 여기서 사용한 var는 function outer()안에서 선언되었기 때문에
         // console.log(score)에 찍힌다.
         var score = 0;
+        // let score: number; : 숫자형 외에는 할당할 수 없게 만든다.
+
+        // score="30"; : score가 int 타입임을 알고 있기 때문에 오류가 생성
+        // 하지만 var score;로 선언이 되어있는 상태라면 오류가 발생하지 않는다.
+
+        // const는 상수이기 때문에 선언될 때 타입을 선언해줘야 한다.
+        // const score: number;
     }
-    for(var i=0; i<3; i++){
+    for(let i=0; i<3; i++){
         setTimeout(function(){
             
             console.log(i);
